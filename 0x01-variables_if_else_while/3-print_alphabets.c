83ad9cb0e36c25ab6e02ce5c80249e0a50934729
deami2023 <glam4damsel@yahooo.com>
2023-04-21 17:29:22 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,14 +6,13 @@
  */
 int main(void)
 {
-	char ch;
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
 
-	for (ch = 'a'; ch <= 'z' ch++)
-		putchar(ch);
-
-	for (ch = 'A'; ch <= 'Z' ch++)
-                putchar(ch);
-
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
 	putchar('\n');
 	return (0);
 }
