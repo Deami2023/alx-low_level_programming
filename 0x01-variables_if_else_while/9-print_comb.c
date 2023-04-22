8ec408bf05df9733e2374e42534f0a9cfb578810
deami2023 <glam4damsel@yahooo.com>
2023-04-21 20:08:05 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,7 @@
 
 /**
  * main - prints comb of numbers.
- * Return: Always 0 (success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
@@ -11,6 +11,10 @@ int main(void)
 	for (n = 48; n <= 57; n++)
 	{
 		putchar(n);
+		if (n == 57)
+		{
+			continue;
+		}
 		putchar(',');
 		putchar(' ');
 	}
