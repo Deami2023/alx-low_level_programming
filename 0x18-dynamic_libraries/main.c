89d8be1c23ff34650d280c394246af00076255c2
Deami2023 <glam4damsel@yahoo.com>
2023-10-20 19:07:42 -0700

    updated

---
+++ 0x18-dynamic_libraries/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - checks code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	printf("%d\n", _strlen("My Dyn Lib"));
+	return (EXIT_SUCCESS);
+}
